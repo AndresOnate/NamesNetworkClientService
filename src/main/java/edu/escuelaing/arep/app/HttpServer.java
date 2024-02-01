8fc990ec9335dfa1a4fec5838e5159e35e043b77
AndresOnate <andrescamiloquimbayo@gmail.com>
2024-02-01 15:51:23 -0500

    Leer del disco

--- src/main/java/edu/escuelaing/arep/app/HttpServer.java
+++ src/main/java/edu/escuelaing/arep/app/HttpServer.java
@@ -9,6 +9,10 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.URI;
 import java.net.URL;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /**
  * Hello world!
@@ -57,9 +61,11 @@ public class HttpServer
                     break;
                 }
             }
-            if(uriStr.startsWith("/cliente")){
-                outputLine = httpClientHtml();
-            }else{
+            URI requestURI = new URI(uriStr);
+            try {
+                outputLine = httpResponse(requestURI);
+            }catch (Exception e){
+                e.printStackTrace();
                 outputLine = httpError();
             }
 
@@ -75,79 +81,96 @@ public class HttpServer
         serverSocket.close();
     }
 
+    public static String httpResponse(URI requestedURI) throws IOException{
+        Path file = Paths.get("target/classes/public" + requestedURI.getPath());
+        String outputLine =
+                "HTTP/1.1 200 OK\r\n"
+                        + "Content-Type:text/html\r\n"
+                        + "\r\n";
+        Charset charset = Charset.forName("UTF-8");
+        BufferedReader reader = Files.newBufferedReader(file, charset);
+        String line = null;
+        while ((line = reader.readLine()) != null) {
+            System.out.println(line);
+            outputLine = outputLine + line;
+        }
+        return  outputLine;
+    }
+
+
     public static String httpClientHtml(){
 
-        String outputLine = 
-        "HTTP/1.1 200 OK\r\n"
-        + "Content-Type:text/html\r\n"
-        + "\r\n"
-        + "<!DOCTYPE html>\n" +
-            "<html>\n" +
-            "    <head>\n" +
-            "        <title>Form Example</title>\n" +
-            "        <meta charset=\"UTF-8\">\n" +
-            "        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n" +
-            "    </head>\n" +
-            "    <body>\n" +
-            "        <h1>Form with GET</h1>\n" +
-            "        <form action=\"/hello\">\n" +
-            "            <label for=\"name\">Name:</label><br>\n" +
-            "            <input type=\"text\" id=\"name\" name=\"name\" value=\"John\"><br><br>\n" +
-            "            <input type=\"button\" value=\"Submit\" onclick=\"loadGetMsg()\">\n" +
-            "        </form> \n" +
-            "        <div id=\"getrespmsg\"></div>\n" +
-            "\n" +
-            "        <script>\n" +
-            "            function loadGetMsg() {\n" +
-            "                let nameVar = document.getElementById(\"name\").value;\n" +
-            "                const xhttp = new XMLHttpRequest();\n" +
-            "                xhttp.onload = function() {\n" +
-            "                    document.getElementById(\"getrespmsg\").innerHTML =\n" +
-            "                    this.responseText;\n" +
-            "                }\n" +
-            "                xhttp.open(\"GET\", \"/hello?name=\"+nameVar);\n" +
-            "                xhttp.send();\n" +
-            "            }\n" +
-            "        </script>\n" +
-            "\n" +
-            "        <h1>Form with POST</h1>\n" +
-            "        <form action=\"/hellopost\">\n" +
-            "            <label for=\"postname\">Name:</label><br>\n" +
-            "            <input type=\"text\" id=\"postname\" name=\"name\" value=\"John\"><br><br>\n" +
-            "            <input type=\"button\" value=\"Submit\" onclick=\"loadPostMsg(postname)\">\n" +
-            "        </form>\n" +
-            "        \n" +
-            "        <div id=\"postrespmsg\"></div>\n" +
-            "        \n" +
-            "        <script>\n" +
-            "            function loadPostMsg(name){\n" +
-            "                let url = \"/hellopost?name=\" + name.value;\n" +
-            "\n" +
-            "                fetch (url, {method: 'POST'})\n" +
-            "                    .then(x => x.text())\n" +
-            "                    .then(y => document.getElementById(\"postrespmsg\").innerHTML = y);\n" +
-            "            }\n" +
-            "        </script>\n" +
-            "    </body>\n" +
-            "</html>";
+        String outputLine =
+                "HTTP/1.1 200 OK\r\n"
+                        + "Content-Type:text/html\r\n"
+                        + "\r\n"
+                        + "<!DOCTYPE html>\n" +
+                        "<html>\n" +
+                        "    <head>\n" +
+                        "        <title>Form Example</title>\n" +
+                        "        <meta charset=\"UTF-8\">\n" +
+                        "        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n" +
+                        "    </head>\n" +
+                        "    <body>\n" +
+                        "        <h1>Form with GET</h1>\n" +
+                        "        <form action=\"/hello\">\n" +
+                        "            <label for=\"name\">Name:</label><br>\n" +
+                        "            <input type=\"text\" id=\"name\" name=\"name\" value=\"John\"><br><br>\n" +
+                        "            <input type=\"button\" value=\"Submit\" onclick=\"loadGetMsg()\">\n" +
+                        "        </form> \n" +
+                        "        <div id=\"getrespmsg\"></div>\n" +
+                        "\n" +
+                        "        <script>\n" +
+                        "            function loadGetMsg() {\n" +
+                        "                let nameVar = document.getElementById(\"name\").value;\n" +
+                        "                const xhttp = new XMLHttpRequest();\n" +
+                        "                xhttp.onload = function() {\n" +
+                        "                    document.getElementById(\"getrespmsg\").innerHTML =\n" +
+                        "                    this.responseText;\n" +
+                        "                }\n" +
+                        "                xhttp.open(\"GET\", \"/hello?name=\"+nameVar);\n" +
+                        "                xhttp.send();\n" +
+                        "            }\n" +
+                        "        </script>\n" +
+                        "\n" +
+                        "        <h1>Form with POST</h1>\n" +
+                        "        <form action=\"/hellopost\">\n" +
+                        "            <label for=\"postname\">Name:</label><br>\n" +
+                        "            <input type=\"text\" id=\"postname\" name=\"name\" value=\"John\"><br><br>\n" +
+                        "            <input type=\"button\" value=\"Submit\" onclick=\"loadPostMsg(postname)\">\n" +
+                        "        </form>\n" +
+                        "        \n" +
+                        "        <div id=\"postrespmsg\"></div>\n" +
+                        "        \n" +
+                        "        <script>\n" +
+                        "            function loadPostMsg(name){\n" +
+                        "                let url = \"/hellopost?name=\" + name.value;\n" +
+                        "\n" +
+                        "                fetch (url, {method: 'POST'})\n" +
+                        "                    .then(x => x.text())\n" +
+                        "                    .then(y => document.getElementById(\"postrespmsg\").innerHTML = y);\n" +
+                        "            }\n" +
+                        "        </script>\n" +
+                        "    </body>\n" +
+                        "</html>";
         return outputLine;
     }
 
     public static String httpError(){
         String outputLine = "HTTP/1.1 400 Not Found\r\n"
-        + "Content-Type:text/html\r\n"
-        + "\r\n"
-        + "<!DOCTYPE html>\n" +
-            "<html>\n" +
-            "    <head>\n" +
-            "        <title>Form Example</title>\n" +
-            "        <meta charset=\"UTF-8\">\n" +
-            "        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n" +
-            "    </head>\n" +
-            "    <body>\n" +  
-                    "<h1>Error</h1>" +         
-            "    </body>\n" +
-            "</html>";
+                + "Content-Type:text/html\r\n"
+                + "\r\n"
+                + "<!DOCTYPE html>\n" +
+                "<html>\n" +
+                "    <head>\n" +
+                "        <title>Form Example</title>\n" +
+                "        <meta charset=\"UTF-8\">\n" +
+                "        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n" +
+                "    </head>\n" +
+                "    <body>\n" +
+                "<h1>Error</h1>" +
+                "    </body>\n" +
+                "</html>";
         return outputLine;
     }
 }
